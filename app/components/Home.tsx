7f540fa14422ea8390dc7bc125ad2796a562c479
lparolari <luca.parolari23@gmail.com>
2020-12-04 14:31:37 +0100

    refactor(dashboard): rename component folder

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,7 +2,7 @@ import { Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
-import Dashboard from './dashboard/Dashboard';
+import Dashboard from './Dashboard/Dashboard';
 
 export default function Home(): JSX.Element {
   return <Dashboard />;
