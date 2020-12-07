09f3dd82958a0a5b389019e6279e66541a35bc7d
lparolari <luca.parolari23@gmail.com>
2020-12-07 18:43:39 +0100

    feat(menu): add screens for route to

---
+++ app/screens/MediaScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+export default function MediaScreen() {
+  return <Typography>Media page</Typography>;
+}
---
+++ app/screens/SystemScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+export default function SystemScreen() {
+  return <Typography>System page</Typography>;
+}
