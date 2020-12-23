24aad589d256d0f943a4f37223360502d40b3760
lparolari <luca.parolari23@gmail.com>
2020-12-23 17:05:13 +0100

    feat(system): clean screen

--- app/screens/SystemScreen.tsx
+++ app/screens/SystemScreen.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 
 import PaddedContainer from '../components/Container/Container';
-import ExternalUrl from '../components/ExternalUrl/ExternalUrl';
 import SystemTabs from './System/Tabs';
 
 export default function SystemScreen() {
   return (
     <PaddedContainer>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Typography paragraph>TODO</Typography>
-          <Typography paragraph>TODO</Typography>
-          <Typography paragraph>
-            Vedi la{' '}
-            <ExternalUrl url={new URL('http://a.com')}>
-              documentazione ufficiale
-            </ExternalUrl>
-            .
-          </Typography>
-        </Grid>
         <Grid item xs={12}>
           <SystemTabs />
         </Grid>
