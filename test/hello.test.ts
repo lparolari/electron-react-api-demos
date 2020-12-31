192256024b31d031a8cafac60b78e31603e8b93b
lparolari <luca.parolari23@gmail.com>
2020-12-31 18:00:17 +0100

    chore(test): remove old test

---
+++ test/hello.test.ts
@@ -0,0 +1,6 @@
+// The application, unfortunately, isn't tested.
+// See issue #3. (https://github.com/lparolari/electron-react-api-demos/issues/3).
+
+it('there is no test here :(', () => {
+  expect(1).toBe(1);
+});
