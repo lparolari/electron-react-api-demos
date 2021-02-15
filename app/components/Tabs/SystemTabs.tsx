8ed60da3a31825a2648c884455edbf3c91a47dd3
lparolari <luca.parolari23@gmail.com>
2021-02-15 21:58:00 +0100

    fix: remove protocol handler

--- app/components/Tabs/SystemTabs.tsx
+++ app/components/Tabs/SystemTabs.tsx
@@ -6,7 +6,6 @@ import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import InfoTab from '../../features/AppSystemInfo/Tab';
 import CopyPasteTab from '../../features/CopyPaste/Tab';
-import ProtocolHandlerTab from '../../features/ProtocolHandler/Tab';
 
 export default function SystemTabs() {
   const [tab, setTab] = React.useState(0);
@@ -19,13 +18,11 @@ export default function SystemTabs() {
         <Tabs value={tab} onChange={handleTabSelect}>
           <Tab label="Info app e sistema" />
           <Tab label="Copia &amp; Incolla" />
-          <Tab label="Protocol Handler" />
         </Tabs>
       </AppBar>
 
       <InfoTab value={tab} index={0} />
       <CopyPasteTab value={tab} index={1} />
-      <ProtocolHandlerTab value={tab} index={2} />
     </Paper>
   );
 }
